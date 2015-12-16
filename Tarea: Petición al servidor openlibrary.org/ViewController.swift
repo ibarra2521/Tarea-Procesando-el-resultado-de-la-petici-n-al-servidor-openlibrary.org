49a0134382a19f4a5d32c58e6903fa3acfc3958d
ibarra2521 <ibarra2521@gmail.com>
2015-12-16 11:19:56 -0500

    First version

--- Tarea: Petición al servidor openlibrary.org/ViewController.swift
+++ Tarea: Petición al servidor openlibrary.org/ViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  Tarea: Petición al servidor openlibrary.org
@@ -11,7 +12,9 @@ import SystemConfiguration
 
 class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var txtfISBN: UITextField!
-    @IBOutlet weak var txtvResult: UITextView!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblAuthors: UILabel!
+    @IBOutlet weak var imgCover: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +28,12 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+    func resetElements() {
+        self.lblTitle.text = ""
+        self.lblAuthors.text = ""
+        self.imgCover.image = UIImage(named: "no found")
+    }
+    
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         //delegate method
         textField.resignFirstResponder()
@@ -32,6 +41,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         if isbn!.characters.count > 0 {
             var url = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
             url = "\(url)\(isbn!)"
+            resetElements()
             requestServer(url)
         }else {
             showAlertMessage("Warning", message: "You must enter the ISBN of the book", owner: self)
@@ -59,17 +69,52 @@ class ViewController: UIViewController, UITextFieldDelegate {
         if isConnectedToNetwork() {
             let urls = urlReques
             let url = NSURL(string: urls)
-            let datos:NSData? = NSData(contentsOfURL: url!)
-            let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
-            self.txtvResult.text = texto! as String
+            let data = NSData(contentsOfURL: url!)
+            do {
+                let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableLeaves)
+                let dictionary1 = json as! NSDictionary
+
+                let isbn = "ISBN:" + self.txtfISBN.text!
+                if dictionary1[isbn] != nil {
+                    print("dictionary1[isbn]: \(dictionary1[isbn])")
+                    let dictionary2 = dictionary1[isbn] as! NSDictionary
+                    
+                    let title = dictionary2["title"] as! NSString as String
+                    self.lblTitle.text = title
+                    
+                    let responseData: NSArray = dictionary2.valueForKey("authors") as! NSArray
+                    var author = ""
+                    for currentAuthor in responseData {
+                        let cAuthor: NSDictionary = currentAuthor as!NSDictionary;
+                        author +=  cAuthor.valueForKey("name") as! String + " - "
+                    }                    
+                    self.lblAuthors.text = author
+                    
+                    let image: String? = dictionary2.valueForKey("cover")?.valueForKey("medium") as? String
+                    
+                    if image != nil {
+                        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+                            let imageData = NSData(contentsOfURL: NSURL(string:image!)!);
+                            dispatch_async(dispatch_get_main_queue(), {
+                                self.imgCover.image = UIImage(data: imageData!);
+                            });
+                        });
+                    }else {
+                        self.imgCover.image = UIImage(named: "no found")   
+                    }
+
+                }else {
+                    resetElements()
+                    showAlertMessage("Warning", message: "ISBN not found", owner: self)
+                }
+                
+            }catch {
+                return
+            }
+            
         }else {
             showAlertMessage("Error", message: "There are problems connecting to the Internet", owner: self)
         }
-    }
-    
-    @IBAction func btnClear(sender: AnyObject) {
-        txtvResult.text = ""
-        txtfISBN.text = ""
     }
     
     func showAlertMessage (title: String, message: String, owner:UIViewController) {
@@ -77,6 +122,10 @@ class ViewController: UIViewController, UITextFieldDelegate {
         alert.addAction(UIAlertAction(title: "Accept", style: UIAlertActionStyle.Default, handler:{ (ACTION :UIAlertAction!)in
         }))
         self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func parsingJson () {
+        
     }
 
 }
